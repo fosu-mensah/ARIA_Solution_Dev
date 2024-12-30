32e8fefc554cff637cf18d99857d400230202516
Timothy <minorgeniuss@naver.com>
2024-12-30 16:46:35 +0900

    init

---
+++ ARIA_Be/src/main/java/aria/license/AriaBeApplication.java
@@ -0,0 +1,13 @@
+package aria.license;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AriaBeApplication.class, args);
+    }
+
+}
---
+++ ARIA_Be/src/test/java/aria/license/AriaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package aria.license;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AriaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
