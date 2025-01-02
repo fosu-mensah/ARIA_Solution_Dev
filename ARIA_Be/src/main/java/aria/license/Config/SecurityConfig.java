683efdae3bdaeac6caa8555b12a9aba12d42a776
genmu5 <genmu4@skuniv.ac.kr>
2025-01-02 16:09:24 +0900

    chore : CSRF customer company 허용

--- ARIA_Be/src/main/java/aria/license/Config/SecurityConfig.java
+++ ARIA_Be/src/main/java/aria/license/Config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/auth/**","/api/users/register").permitAll()
+                    .antMatchers("/auth/**","/api/users/register","/customer-company/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
